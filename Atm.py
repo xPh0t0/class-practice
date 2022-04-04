20a6b6ecc6582c3679852bc62b4b93fca6514136
xPh0t0 <52384505+xPh0t0@users.noreply.github.com>
2022-04-04 06:56:49 -0400

    Add files via upload

---
+++ Atm.py
@@ -0,0 +1,20 @@
+class Atm:
+    def __init__(self, cardNumber, pin, expirationDate):
+        self.cardNumber = cardNumber
+        self.pin = pin
+        self.expirationDate = expirationDate
+
+    def card_number(self):
+        return("Your card number is ", self.cardNumber)
+
+    def Pin(self):
+        return("Your pin is ", self.pin)
+
+    def expiration_date(self):
+        return("Your card expires on ", self.expirationDate)
+
+a = Atm(12345678910, 9876, "January 1st, 2025")
+
+print(a.cardNumber)
+print(a.pin)
+print(a.expirationDate)
